1a71e135ece7c126c7edd44aa8dbbf72061f6e60
Joel Jebitto <joeljebitto@gmail.com>
2025-05-24 22:30:09 +0530

    Added Exception handling for Student

--- src/main/java/com/joeljebitto/demo/rest/StudentRestController.java
+++ src/main/java/com/joeljebitto/demo/rest/StudentRestController.java
@@ -7,6 +7,9 @@ import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +38,27 @@ public class StudentRestController {
 
   @GetMapping("/students/{studentId}")
   public Student getStudent(@PathVariable int studentId) {
-
+    if ((studentId < 0) || (studentId >= theStudents.size())) {
+      throw new StudentNotFoundException("Student id not found - " + studentId);
+    }
     return theStudents.get(studentId);
+  }
+
+  @ExceptionHandler
+  public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+    StudentErrorResponse error = new StudentErrorResponse();
+    error.setStatus(HttpStatus.NOT_FOUND.value());
+    error.setMessage(exc.getMessage());
+    error.setTimeStamp(System.currentTimeMillis());
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler
+  public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+    StudentErrorResponse error = new StudentErrorResponse();
+    error.setStatus(HttpStatus.BAD_REQUEST.value());
+    error.setMessage(exc.getMessage());
+    error.setTimeStamp(System.currentTimeMillis());
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
   }
 }
